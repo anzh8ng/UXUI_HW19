1f7179dee7049a221be14c79e9b23474075c6f6e
anzh8ng <70453300+anzh8ng@users.noreply.github.com>
2022-04-29 00:41:54 -0400

    click function

---
+++ code/js/index.js
@@ -0,0 +1,18 @@
+console.log("correct");
+
+$( "#About" ).click(function() {
+    alert( "About was clicked" );
+  });
+
+  $( "#Skills" ).click(function() {
+    alert( "Skills was clicked" );
+  });
+
+  $( "#Work" ).click(function() {
+    alert( "Work was clicked" );
+  });
+
+  $( "#Contact" ).click(function() {
+    alert( "Contact was clicked" );
+  });
+
